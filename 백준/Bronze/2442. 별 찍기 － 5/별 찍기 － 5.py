f5de47e7562c625020266ba72e363df9c40ad17c
Eunji13 <105190637+dmswl0103@users.noreply.github.com>
2025-04-01 23:38:20 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end='')
+    for k in range(2*i+1):
+        print("*",end='')
+    print("")
